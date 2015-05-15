6b6217181d3c8479caaf3bf144feeb4d6f2dfbe2
Ghislaine Guerin <me@ghislaineguerin.com>
2015-05-15 07:53:38 -0400

    prototype folder structure

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ var MockupsApp = angular.module('MockupsApp', [
   'MockupsApp.directives'
 ]);
 
-
 MockupsApp.config(function($stateProvider, $urlRouterProvider) {
   // For any unmatched url, redirect to /
   $urlRouterProvider.otherwise("/");
@@ -21,4 +20,33 @@ MockupsApp.config(function($stateProvider, $urlRouterProvider) {
     ;     
 });
 
+MockupsApp.controller('MockupsAppController', function($scope) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
+  ];
+});
+
+MockupsApp.controller('DropdownCtrl', function ($scope, $log) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
+  ];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+});
 
