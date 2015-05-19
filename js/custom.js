cea65126a56e55e7365fe4fb41d760a3210a58d3
RV-Adrian <adrian.dalimonte@risevision.com>
2015-05-19 14:02:56 -0400

    added Subscription statuses

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,33 @@
 
 
 
-function showFileInspector() {
+function showFileInspector_noFiles() {
     $('#pitchdeck').hide();
-    $('#fileinspector').show();
+    $('#fileinspector_noFiles').show();
     $('.global-actions').show();
 
 }
 
+
+function showFileInspector_withFiles() {
+    $('#pitchdeck').hide();
+    $('#fileinspector_noFiles').hide();
+    $('#fileinspector_withFiles').show();
+    $('.global-actions').show();
+    $('.uploadCTA').hide();
+
+}
+
+function showNextSubStatus() {
+
+  var activeStat = $('.subStat.active');
+
+  $(activeStat).removeClass('active');
+  $(activeStat).next().addClass('active');
+}
+
+function loadSubscribed() {
+
+    $('.subStat.active').removeClass('active');
+	$('#subscriberStatus').show();
+}
