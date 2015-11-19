b54e2104783826969b185ac8e3e8cdaa47507a3a
Donna Peplinskie <donna.peplinskie@risevision.com>
2015-11-19 11:31:47 -0500

    Support for automated deployment
    Use exit code 0 for npm run test

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,86 @@
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var jsoncombine = require("gulp-jsoncombine");
-var colors = require('colors');
-var watch = require('gulp-watch');
-var rename = require('gulp-rename');
-var mainBowerFiles = require('main-bower-files');
-var clean = require('gulp-clean');
+(function (console) {
+  "use strict";
 
-gulp.task('dev', function() {
-  // Start a server
-  connect.server({
-    root: '',
-    port: 3000,
-    livereload: true
+  var bower = require("gulp-bower");
+  var bump = require("gulp-bump");
+  var colors = require("colors");
+  var connect = require("gulp-connect");
+  var del = require("del");
+  var gulp = require("gulp");
+  var jshint = require("gulp-jshint");
+  var runSequence = require("run-sequence");
+  var watch = require("gulp-watch");
+
+  var jsFiles = ["js/**/*.js", "prototypes/**/*.js"],
+    files = ["*.html", "bower_components/**/*", ,
+      "partials/**/*.html", "prototypes/**/*.html", "prototypes/**/*.css"];
+
+  gulp.task("bower-clean-install", ["clean-bower"], function(cb) {
+    return bower().on("error", function(err) {
+      console.log(err);
+      cb();
+    });
   });
-  console.log('[CONNECT] Listening on port 3000'.yellow.inverse);
-  // Watch HTML files for changes
-  console.log('[CONNECT] Watching HTML files for live-reload'.blue);
-  watch({
-    glob: ['./partials/**/*.html', './js/**/*.js', './index.html', './style.css', './bower_components/rv-common-style/dist/css/rise.min.css', './distribution/**/*.*']
-  })
-    .pipe(connect.reload());
-});
 
-gulp.task('default', [], function() {
-  console.log('***********************'.yellow);
-  console.log('  gulp dev: start a server in the folder and watch LESS files'.yellow);
-  console.log('***********************'.yellow);
-  return true;
-});
+  gulp.task("build", function(cb) {
+    runSequence(["clean"], ["source", "images"], cb);
+  });
+
+  gulp.task("bump", function() {
+    return gulp.src(["./package.json", "./bower.json"])
+      .pipe(bump({type:"patch"}))
+      .pipe(gulp.dest("./"));
+  });
+
+  gulp.task("clean", function (cb) {
+    del(["./dist/**"], cb);
+  });
+
+  gulp.task("clean-bower", function(cb) {
+    del(["./bower_components/**"], cb);
+  });
+
+  gulp.task("dev", function() {
+    // Start a server
+    connect.server({
+      root: "",
+      port: 3000,
+      livereload: true
+    });
+    console.log("[CONNECT] Listening on port 3000".yellow.inverse);
+    // Watch HTML files for changes
+    console.log("[CONNECT] Watching HTML files for live-reload".blue);
+    watch({
+      glob: ["./partials/**/*.html", "./js/**/*.js", "./index.html", "./style.css",
+        "./bower_components/rv-common-style/dist/css/rise.min.css", "./distribution/**/*.*"]
+      })
+      .pipe(connect.reload());
+  });
+
+  gulp.task("lint", function() {
+    return gulp.src(jsFiles)
+      .pipe(jshint())
+      .pipe(jshint.reporter("jshint-stylish"))
+      .pipe(jshint.reporter("fail"));
+  });
+
+  gulp.task("source", ["lint"], function () {
+    return gulp.src(files.concat(jsFiles), { base: "./" })
+      .pipe(gulp.dest("dist/"));
+  });
+
+  gulp.task("images", function() {
+    return gulp.src(["img/**/*.*", "prototypes/**/img/**/*.*"], { base: "./" })
+      .pipe(gulp.dest("dist/"));
+  });
+
+  gulp.task("default", [], function() {
+    console.log("***********************".yellow);
+    console.log("  gulp bower-clean-install: delete and re-install bower components".yellow);
+    console.log("  gulp bump: bump the version number".yellow);
+    console.log("  gulp dev: start a server in the folder and watch LESS files".yellow);
+    console.log("  gulp build: build a distribution version".yellow);
+    console.log("***********************".yellow);
+    return true;
+  });
+})(console);
--- js/app.js
+++ js/app.js
@@ -1,65 +1,65 @@
-'use strict';
+(function() {
+  "use strict";
 
-angular.module('MockupsApp.directives', []);
+  angular.module("MockupsApp.directives", []);
 
-var MockupsApp = angular.module('MockupsApp', [
-  'ngRoute',
-  'ngTouch',
-  'ui.router',
-  'ui.bootstrap',
-  'MockupsApp.directives'
-]);
+  var MockupsApp = angular.module("MockupsApp", [
+    "ngRoute",
+    "ngTouch",
+    "ui.router",
+    "ui.bootstrap",
+    "MockupsApp.directives"
+  ]);
 
-MockupsApp.config(function($stateProvider, $urlRouterProvider) {
-  // For any unmatched url, redirect to /
-  $urlRouterProvider.otherwise("/");
-  //
-  // Now set up the states
-  $stateProvider
-      .state('/', {url: "/",templateUrl: ""})
-    ;     
-});
-
-MockupsApp.controller('MockupsAppController', function ($scope, $modal, $log) {
-
-  $scope.a = {
-  "Statuses":
-  [
-    {"ID":"1", "Name":"SignedOut"},
-    {"ID":"2", "Name":"SignedIn"}
-  ],
-  "StatusID":"1"
-  };
-  $scope.StatusID=1;
-
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size) {
-
-  var modalInstance = $modal.open({
-    animation: $scope.animationsEnabled,
-    templateUrl: 'myModalContent.html',
-    controller: 'ModalInstanceCtrl',
-    size: size,
-    resolve: {
-      items: function () {
-      return $scope.items;
-      }
-    }
+  MockupsApp.config(function($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, redirect to /
+    $urlRouterProvider.otherwise("/");
+    //
+    // Now set up the states
+    $stateProvider
+        .state("/", {url: "/",templateUrl: ""})
+      ;
   });
 
-  modalInstance.result.then(function (selectedItem) {
-    $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
+  MockupsApp.controller("MockupsAppController", function ($scope, $modal, $log) {
+
+    $scope.a = {
+    "Statuses":
+    [
+      {"ID":"1", "Name":"SignedOut"},
+      {"ID":"2", "Name":"SignedIn"}
+    ],
+    "StatusID":"1"
+    };
+    $scope.StatusID=1;
+
+    $scope.items = ["item1", "item2", "item3"];
+
+    $scope.animationsEnabled = true;
+
+    $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: "myModalContent.html",
+      controller: "ModalInstanceCtrl",
+      size: size,
+      resolve: {
+        items: function () {
+        return $scope.items;
+        }
+      }
     });
-  };
 
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+      }, function () {
+        $log.info("Modal dismissed at: " + new Date());
+      });
+    };
 
-
-});
+    $scope.toggleAnimation = function () {
+      $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+  });
+}());
